5d30c7851de48c649d867f7594d180c96cf87151
A-Beni <a.ngoga@alustudent.com>
2023-09-17 09:42:56 -0700

    Alu-Tasks

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,28 +1,20 @@
 #!/usr/bin/python3
-"""
-0x03. Minimum Operations - dertimines minimum number of "copy all"/"paste"
-operations to produce `n` number characters starting from 1.
-"""
+"""Minimum Operations"""
+import math
 
 
 def minOperations(n):
-
-    if (type(n) != int):
+    """Minimum Operations"""
+    if n <= 1:
         return 0
 
-    # n == 1 returns 0 steps as it is the starting state
-    if (n < 2):
-        return 0
-
-    minOps = 0
-
-    # within the potential factors of n, aside from 1
-    for i in range(2, (n + 1)):
-        # for each factor found
-        while (n % i == 0):
-            # add that to the total operations
-            minOps += i
-            # refocus to consider the remainder unprinted
+    operations = 0
+    for i in range(2, int(math.sqrt(abs(n))) + 1):
+        while n % i == 0:
+            operations += i
             n //= i
 
-    return minOps
+    if n > 1:
+        operations += n
+
+    return operations
